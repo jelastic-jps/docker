55c966a106fa8e258e7e8bbfa5d027faed86ae4b
alexey.lazarenko <dzotic9@gmail.com>
2020-09-14 12:13:49 +0300

    JE-54953 [Docker Engine][LE]: LE-scripts are removed during redeploy

--- addons/le-ssl-deploy-hook.js
+++ addons/le-ssl-deploy-hook.js
@@ -1,11 +1,17 @@
+nodeId = getParam('nodeId');
 envName = getParam('envName');
 envDomain = getParam('envDomain');
 scriptName = getParam('action') == 'uninstall' ? 'undeployLE.sh' : 'deployLE.sh';
+
+resp = jelastic.environment.control.GetNodeInfo("${env.appid}", session, nodeId);
+if (resp.result != 0) return resp;
+
+if (resp.node && resp.node.nodeGroup == "bl") return {result: 0}
 
 //getting first custom domain
 customDomains = (getParam('customDomains') || "").replace(/^\s+|\s+$/gm , "").split(/\s*[;,\s]\s*/).shift(); 
 domain = customDomains || envDomain;
 
 //executing custom deployment hook script on master node
-resp = jelastic.env.control.ExecCmdById(envName, session, getParam('nodeId'), toJSON([{ command:'/bin/bash ' + scriptName}]), true);
+resp = jelastic.env.control.ExecCmdById(envName, session, nodeId, toJSON([{ command:'/bin/bash ' + scriptName}]), true);
 return resp;
